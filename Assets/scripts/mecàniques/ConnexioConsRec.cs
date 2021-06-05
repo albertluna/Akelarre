b273187cb80815da835a26af4961c2eb75a27482
albert <albert.luna@students.salle.url.edu>
2021-06-05 20:32:29 +0200

    connexió recol·lector - constructor v3.1

--- Assets/scripts/mecàniques/ConnexioConsRec.cs
+++ Assets/scripts/mecàniques/ConnexioConsRec.cs
@@ -26,7 +26,7 @@ public class ConnexioConsRec : MonoBehaviour
     [PunRPC]
     private void RPC_sendColleccionable(string colleccionable)
     {
-        Debug.Log("ENVIA YEAHHH");
+        Debug.Log("ENVIA YEAHHH " + constructor.name);
         constructor.CrearColleccionable(colleccionable);
     }
 }
