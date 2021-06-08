c5efe20f74515bef27cb5f8e3a9c98d9909329f6
albert <albert.luna@students.salle.url.edu>
2021-06-08 19:01:39 +0200

    realtime atac instanciador v1

--- Assets/scripts/mecàniques/Defensor/atacController.cs
+++ Assets/scripts/mecàniques/Defensor/atacController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class atacController : MonoBehaviour
 {
+    public GameObject AC;
+
     public Transform[] creators;
     public GameObject bullet;
     public float timer;
     public float maxEspera;
     public float minEspera;
+    //public GameObject llistaCreators;
+    public GameObject instanciador;
+    [SerializeField]
+    private PhotonView PV;
 
     /// <summary> array de gameobjects
     /// https://stuartspixelgames.com/2017/08/02/make-all-of-objects-children-into-an-array-unity-c/
@@ -16,23 +23,35 @@ public class atacController : MonoBehaviour
     /// 
     void Start()
     {
-        creators = GetComponentsInChildren<Transform>();
+        AC = GameObject.Find("creadorsAtac");
+        instanciador = GameObject.Find("instanciador");
+        creators = AC.GetComponentsInChildren<Transform>();
+        PV = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //https://stackoverflow.com/questions/33182283/how-do-i-create-random-game-objects-at-runtime-in-unity-2d-using-c
-        if (timer >= 0)
-        {
-            timer -= Time.deltaTime;
+        if (PV.IsMine) {
+            //https://stackoverflow.com/questions/33182283/how-do-i-create-random-game-objects-at-runtime-in-unity-2d-using-c
+            if (timer >= 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                int posicio = Random.Range(1, creators.Length);
+                PV.RPC("RPC_instanciarAtac", RpcTarget.All, posicio);
+                timer = Random.Range(minEspera, maxEspera);
+            }
         }
-        else
-        {
-            int posicio = Random.Range(1, creators.Length);
-            GameObject instancia = Instantiate(bullet, creators[posicio].position, Quaternion.identity);
-            instancia.GetComponent<MovimentAtac>().parent = this.gameObject;
-            timer = Random.Range(minEspera, maxEspera);
-        }
+    }
+
+    [PunRPC]
+    private void RPC_instanciarAtac(int posicio)
+    {
+        GameObject instancia = Instantiate(bullet, creators[posicio].position, Quaternion.identity, instanciador.transform);
+        instancia.GetComponent<MovimentAtac>().parent = this.gameObject;
+        Debug.Log("INSTANCIAT");
     }
 }
