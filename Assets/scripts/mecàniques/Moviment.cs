8e8291fc6a4ad19de91bc323ef76f869ca0e7beb
albert <albert.luna@students.salle.url.edu>
2021-06-20 12:32:55 +0200

    rotacio moviment player

--- Assets/scripts/mecàniques/Moviment.cs
+++ Assets/scripts/mecàniques/Moviment.cs
@@ -9,7 +9,7 @@ public class Moviment : MonoBehaviourPunCallbacks, IPunObservable
     public PhotonView PV;
     private bool controllable = true;
 
-    public float MaxSpeed = 0.2f;
+    public float MaxSpeed;
 
     private float horizontalInput;
     private float verticalInput;
@@ -58,11 +58,44 @@ public class Moviment : MonoBehaviourPunCallbacks, IPunObservable
             return;
         }
 
-        Quaternion rot = rigidbody.rotation * Quaternion.Euler(0, horizontalInput * MaxSpeed/100 * Time.fixedDeltaTime, 0);
-        GetComponent<Rigidbody>().MoveRotation(rot);
+        //Quaternion rot = rigidbody.rotation * Quaternion.Euler(0, horizontalInput * MaxSpeed/100 * Time.fixedDeltaTime, 0);
+        //GetComponent<Rigidbody>().MoveRotation(rot);
 
-        Vector3 force = (rot * Vector3.forward) * verticalInput * 10000.0f * MaxSpeed * Time.fixedDeltaTime;
-        GetComponent<Rigidbody>().AddForce(force);
+        //Vector3 force = (rot * Vector3.forward) * verticalInput * 10000.0f * MaxSpeed * Time.fixedDeltaTime;
+        //GetComponent<Rigidbody>().AddForce(force);
+
+
+        /*Vector3 projeccio = new Vector3();
+        projeccio = (camera.transform.position - this.transform.position);
+        projeccio.y = 0;
+        Debug.Log("PROJECCIO = " + projeccio.normalized);
+        Vector3 dir = new Vector3(horizontalInput, 0, verticalInput);
+
+        float dif = Vector3.Angle(projeccio, new Vector3(1,0,0));*/
+
+        var forward = camera.transform.forward;
+        var right = camera.transform.right;
+
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        //this is the direction in the world space we want to move:
+        var desiredMoveDirection = right * verticalInput  - forward * horizontalInput;
+        Debug.Log("Moviment " + desiredMoveDirection);
+
+        //now we can apply the movement:
+        //transform.Translate(desiredMoveDirection * velocity * Time.deltaTime);
+
+        //Quaternion.Lo
+        if (desiredMoveDirection.magnitude > 0.1)
+        {
+            GetComponent<Rigidbody>().MoveRotation(Quaternion.LookRotation(-desiredMoveDirection, Vector3.up));
+        }
+        //this.transform.LookAt(tmp);
+
 
         if (GetComponent<Rigidbody>().velocity.magnitude > MaxSpeed)
         {
