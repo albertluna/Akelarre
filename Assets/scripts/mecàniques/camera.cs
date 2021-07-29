551f62aca8fcd059cf923ad3ea0aa01d35f88161
albert <albert.luna@students.salle.url.edu>
2021-07-29 10:47:01 +0200

    ultims canvis

--- Assets/scripts/mecàniques/camera.cs
+++ Assets/scripts/mecàniques/camera.cs
@@ -19,7 +19,11 @@ public class camera : MonoBehaviour
         //cinematiques = GetComponent<Animation>();
         yOriginal = this.transform.position.y;
         offset = Vector3.Distance(this.transform.position, jugador.transform.position);
+        Vector3 dir = jugador.transform.position.normalized;
+
+        transform.position = new Vector3(dir.x * radi, yOriginal + (radi), dir.z * radi);
     }
+#if UNITY_STANDALONE
 
     // Update is called once per frame
     void FixedUpdate()
@@ -32,6 +36,20 @@ public class camera : MonoBehaviour
         transform.position = posicioIntermitja;
         transform.LookAt(jugador.transform.position);
     }
+#endif
+#if UNITY_ANDROID
+// Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = jugador.transform.position.normalized;
+        float distanciaAlCentre = Vector3.Distance(jugador.transform.position, new Vector3(0, 0, 0));
+        radi = distanciaAlCentre + offset;
+        Vector3 posicioDesitjada = new Vector3(dir.x*radi, yOriginal+(radi), dir.z*radi); //posar a y distanciaAlCentre
+        Vector3 posicioIntermitja = Vector3.Lerp(this.transform.position, posicioDesitjada, movimentSuau);
+        transform.position = posicioIntermitja;
+        transform.LookAt(jugador.transform.position);
+    }
+#endif
 
     public void setJugador(GameObject jugador)
     {
